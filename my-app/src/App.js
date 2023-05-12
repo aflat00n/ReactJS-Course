db47a77bbec0b6381dc1793daad9424e86790fc1
Omar Sohail <omarsohail@gmail.com>
2023-05-13 00:02:12 +0500

    adding some logic, colors & using states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,13 @@ import TextForm from './components/TextForm';
 function App() {
   return (
     <>
-    {/* <Navbar title='Text Utility' home='Home' about='About'/> */}
-    <Navbar/>
-    <div className="container my-3">
-    <TextForm heading='Enter text to analyze'/>
-    </div>
+      <div className="pagecontainer">
+        {/* <Navbar title='Text Utility' home='Home' about='About'/> */}
+        <Navbar/>
+        <div className="container my-3">
+        <TextForm heading='Enter text to analyze'/>
+        </div>
+      </div>
     </>
   );
 }
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -6,20 +6,32 @@ export default function TextForm(props) {
         let newText = text.toUpperCase()
         setText(newText)
     }
+    const handleLowClick = ()=>{
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
     const handleOnChange = (event)=>{
         setText(event.target.value)
     }
-    const formClick = ()=>{
-        setText('')
-    }
-    const [text, setText] = useState('Enter Text Here');
+    // const formClick = ()=>{
+    //     setText('')
+    // }
+    const [text, setText] = useState('');
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-        <textarea className="form-control" value={text} onClick={formClick} onChange={handleOnChange} id="box" rows="8"></textarea>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+    <>
+        <h1 className='text-center'>{props.heading}</h1>
+        <div className="mb-3 text-center">
+            <textarea className="form-control" value={text} placeholder='Enter Text Here' onChange={handleOnChange} id="box" rows="8"></textarea>
+            <button className="btn btn-outline-primary mx-1 my-2" onClick={handleUpClick}>Upper Case</button>
+            <button className="btn btn-outline-primary mx-1 my-2" onClick={handleLowClick}>Lower Case</button>
         </div>
-    </div>
+        <div className="container my-3 text-center">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(' ').length} words | {text.length} characters</p>
+            <p>Approx. Reading Time: {Math.round(text.split(' ').length * 0.008)} minutes</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+    </>
   )
 }
